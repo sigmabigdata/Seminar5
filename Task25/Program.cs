d703ab60e23dd857655fcdbf4bf93869197bd25f
anatoli dyrul <dyrulan@gmail.com>
2023-01-31 22:29:56 +0300

    task complite

---
+++ Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+
+    result = a * result;
+
+}
+
+
+
+
+
+Console.WriteLine($"{a} в степени {b} = {result}");
---
+++ Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+    string str = number.ToString();
+    {
+        int sum = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            string str2 = Convert.ToString(str[i]);
+            sum = sum + int.Parse(str2);
+        }
+    
+
+        Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
+    }
